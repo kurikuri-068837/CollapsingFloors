6af41c972a6122e377e908db0760e834c51e679e
kurikuri-068837 <104060772+kurikuri-068837@users.noreply.github.com>
2022-09-16 13:39:00 +0900

    prefab

---
+++ Assets/Tests/Stage/StageScript/StageColliderJudgement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageColliderJudgement : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit"); // ÉçÉOÇï\é¶Ç∑ÇÈ
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
